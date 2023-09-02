49d3a8c50b1565b3b4188507a4019e34c29a7af4
MANUHYA <143479670+MANUHYA@users.noreply.github.com>
2023-09-02 17:31:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import pandas as pd
 import joblib as jb
 
 
-
 def return_df(age,sex,bmi,children,smoker,region):
 	kbn={
 	'age':[age],
@@ -26,7 +25,7 @@ st.markdown(f'''
 ''',unsafe_allow_html=True)
 age=st.number_input('Enter your age',min_value=0)
 sex=st.selectbox('Select your gender',['male','female'])
-bmi=st.number_input('body mass index',min_value=0)
+bmi=st.number_input('bmi',min_value=0)
 children=st.number_input('Count of your Children',min_value=0)
 smoker=st.selectbox(' Do you smoke',['yes','no'])
 region=st.selectbox('Select your Region',['southeast','southwest','northeast','northwest'])
@@ -36,6 +35,6 @@ if st.button('Get your charges'):
 	model=base_model()
 	preds=model.predict(df)
 	predictions=preds[0]
-	st.write(predictions)
+	st.write('Individual medical cost billed by health insurance is: {}$'.format(predictions))
 
 
